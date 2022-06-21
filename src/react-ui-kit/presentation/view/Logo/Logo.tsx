64f486ebb1e6b711f9fa1c1af6336a795606cdce
Vladislav Polekha <vladsimplemente@gmail.com>
2022-06-21 17:15:46 +0300

    feat(Logo): add brand title

--- src/react-ui-kit/presentation/view/Logo/Logo.tsx
+++ src/react-ui-kit/presentation/view/Logo/Logo.tsx
@@ -5,15 +5,35 @@ import { styled } from '@linaria/react';
 export type TLogoProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
   width?: string;
   height?: string;
+  brandName?: string;
 };
 
-const Logo = (props: Readonly<TLogoProps>) => <SLogo {...props} />;
+const Logo = ({ brandName, ...restProps }: Readonly<TLogoProps>) => (
+  <SLogoWrapper>
+    <SLogo {...restProps} />
+    {brandName && <SBrand translate="no">{brandName}</SBrand>}
+  </SLogoWrapper>
+);
 
 export default React.memo(Logo);
+
+const SLogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SBrand = styled.span`
+  margin-left: 12px;
+
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 29px;
+`;
 
 const SLogo = styled.img<TLogoProps>`
   position: relative;
   cursor: pointer;
+  transform: translateY(10%);
 
   filter: none !important;
   pointer-events: auto !important;
